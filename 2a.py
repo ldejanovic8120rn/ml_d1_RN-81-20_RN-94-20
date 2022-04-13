5c45c587132bbd463989393a42d33165eb6dbbde
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2022-04-14 01:23:54 +0200

    gmta

--- 2a.py
+++ 2a.py
@@ -16,7 +16,6 @@ skupi_senzori_data = pd.read_csv('./data/skupi_senzori.XLS', sep='\t', skiprows=
 skupi_senzori_data['date beginning'] = skupi_senzori_data['date beginning'] + ' ' + skupi_senzori_data['time beginning']
 skupi_senzori_data['date beginning'] = pd.to_datetime(skupi_senzori_data['date beginning']).dt.strftime('%d-%m-%y %H:%M:%S')
 skupi_senzori_data['date beginning'] = pd.to_datetime(skupi_senzori_data['date beginning']) + pd.DateOffset(hours=-2)
-# skupi_senzori_data['date beginning'].dt.DateOffset(hours=-2)
 skupi_senzori_data.drop('time beginning', axis='columns', inplace=True)
 
 skupi_senzori_data['date end'] = skupi_senzori_data['date end'] + ' ' + skupi_senzori_data['time end']
@@ -24,6 +23,14 @@ skupi_senzori_data['date end'] = pd.to_datetime(skupi_senzori_data['date end']).
 skupi_senzori_data['date end'] = pd.to_datetime(skupi_senzori_data['date end']) + pd.DateOffset(hours=-2)
 skupi_senzori_data.drop('time end', axis='columns', inplace=True)
 
-print(skupi_senzori_data)
 
+# skupi_senzori_data.index = pd.IntervalIndex.from_arrays(skupi_senzori_data['date beginning'], skupi_senzori_data['date end'], closed='both')
+# jeftini_senzori_data['date beggining'] = jeftini_senzori_data['date'].apply(lambda x: skupi_senzori_data.iloc[skupi_senzori_data.index.get_loc(x)]['date beginning'])
+# jeftini_senzori_data['date end'] = jeftini_senzori_data['date'].apply(lambda x: skupi_senzori_data.iloc[skupi_senzori_data.index.get_loc(x)]['date end'])
+# jeftini_senzori_data['reference pm2.5'] = jeftini_senzori_data['date'].apply(lambda x: skupi_senzori_data.iloc[skupi_senzori_data.index.get_loc(x)]['PM2.5_ambient - #11'])
+
+df = pd.concat([jeftini_senzori_data, skupi_senzori_data], axis=1)
+# df = df[(df['date'] >= df['date beginning']) & (df['date'] <= df['date end'])]
+
+print(df)
 # jeftini_senzori_data.to_excel('./data/2aa.xls', index=False, header=False)
